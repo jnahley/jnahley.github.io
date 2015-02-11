a4cadd1b0592c08a59c4201c58cf1769243f4249
Josh Nahley <jnahley@hotmail.com>
2015-02-11 13:04:20 -0500

    Constructed Table with total column.
    No CSS yet

---
+++ ie59000/2015spring/Homework-2/hw2.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $.getJSON("https://hivelab.org/static/students.json",function(grads){
+        var gradTable=$("#gradDataTable");
+        $.each(grads,function(data,grad){
+            var totalScore=(grad.GPA/4)+((grad.GRE_V-130)/40)+((grad.GRE_Q-130)/40)+(grad.Essay/6);
+            var totalPercent=(totalScore/4)*100;
+            
+            
+            gradTable.append(
+                "<tr><td>"+grad.Name+"</td><td>"+grad.GPA+"</td><td>"+grad.GRE_V+"</td><td>"+grad.GRE_Q+"</td><td>"+grad.Essay+"</td><td>"+grad.Recom+"</td><td>"+totalPercent+"</td></tr>");
+        });
+    });
+});
+        
+    
