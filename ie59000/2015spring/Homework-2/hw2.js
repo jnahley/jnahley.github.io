932e7362b44cd91aab07610a4d21054fa39e85b7
Josh Nahley <jnahley@hotmail.com>
2015-02-15 16:40:59 -0500

    Homework 2 Final Changes

--- ie59000/2015spring/Homework-2/hw2.js
+++ ie59000/2015spring/Homework-2/hw2.js
@@ -6,11 +6,9 @@ $(document).ready(function(){
             var totalScore=(grad.GPA/4)+((grad.GRE_V-130)/40)+((grad.GRE_Q-130)/40)+(grad.Essay/6);
             var totalPercent=(totalScore/4)*100;
             var roundedPercent=Math.round(totalPercent*100)/100;
-            var ranks =[];
-            ranks=ranks.push(totalPercent);
-            
-            
-            
+            var i=0;
+            i=i+1;
+            console.log(i);
             
             gradTable.append(
                 "<tr><td>"+grad.Name+"</td><td>"+grad.GPA+"</td><td>"+grad.GRE_V+"</td><td>"+grad.GRE_Q+"</td><td>"+grad.Essay+"</td><td>"+grad.Recom+"</td><td>"+roundedPercent+"%"+"</td></tr>");
